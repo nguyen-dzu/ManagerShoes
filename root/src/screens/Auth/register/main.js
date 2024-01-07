309ef0cf111417b446652a5a9adef9b85eaf7f23
Kyran Nguyen <kyran@vinova.com.sg>
2024-01-07 11:03:25 +0700

    fix

--- root/src/screens/Auth/register/main.js
+++ root/src/screens/Auth/register/main.js
@@ -1,1 +1,9 @@
 console.log("register");
+// đăng ký thành công: uid: 80lctzf3SsUI52rY35R9vkvQfUN2
+// lưu vào firebase realtime database:
+user {
+    id: uid,
+    name, 
+    email,
+    age, fullName, avartar, phoneNumber, address
+}
